48c8f7c737a50a3bf739696447d49cb01d2ecb5b
Shruthi Srinivas <60729418+shruthey@users.noreply.github.com>
2026-09-22 13:25:57 -0600

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+        
+        left = 0
+        for right, c in enumerate(s2, start=len(s1)-1):
+            if Counter(s1) == Counter(s2[left:right+1]):
+                return True
+            else:
+                left += 1
+        return False
